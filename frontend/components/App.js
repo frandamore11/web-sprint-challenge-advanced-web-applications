9ef58e8cfb6169290aa5a344c1b068142bea561a
francesco damore <frandamore11@gmail.com>
2024-11-16 22:15:53 -0600

    worked on App.js

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -5,6 +5,7 @@ import LoginForm from './LoginForm'
 import Message from './Message'
 import ArticleForm from './ArticleForm'
 import Spinner from './Spinner'
+import axios from 'axios'
 
 const articlesUrl = 'http://localhost:9000/api/articles'
 const loginUrl = 'http://localhost:9000/api/login'
@@ -18,8 +19,8 @@ export default function App() {
 
   // ✨ Research `useNavigate` in React Router v.6
   const navigate = useNavigate()
-  const redirectToLogin = () => { /* ✨ implement */ }
-  const redirectToArticles = () => { /* ✨ implement */ }
+  const redirectToLogin = () => { navigate('/')}
+  const redirectToArticles = () => { navigate('/articles') }
 
   const logout = () => {
     // ✨ implement
@@ -27,6 +28,9 @@ export default function App() {
     // and a message saying "Goodbye!" should be set in its proper state.
     // In any case, we should redirect the browser back to the login screen,
     // using the helper above.
+    localStorage.removeItem('token')
+    setMessage('Goodbye!')
+    redirectToLogin()
   }
 
   const login = ({ username, password }) => {
@@ -36,6 +40,20 @@ export default function App() {
     // On success, we should set the token to local storage in a 'token' key,
     // put the server success message in its proper state, and redirect
     // to the Articles screen. Don't forget to turn off the spinner!
+    setMessage('')
+    setSpinnerOn(true)
+    axios.post(loginUrl, { username, password })
+      .then(res => {
+        localStorage.setItem('token', res.data.token)
+        setMessage(res.data.message)
+        redirectToArticles()
+      })
+      .catch(err => {
+        setMessage(err.response?.data?.message || 'Something went wrong')
+      })
+      .finally(() => {
+        setSpinnerOn(false)
+      })
   }
 
   const getArticles = () => {
@@ -47,6 +65,26 @@ export default function App() {
     // If something goes wrong, check the status of the response:
     // if it's a 401 the token might have gone bad, and we should redirect to login.
     // Don't forget to turn off the spinner!
+    setMessage('')
+    setSpinnerOn(true)
+    const token = localStorage.getItem('token')
+    
+    axios.get(articlesUrl, {
+      headers: { Authorization: token }
+    })
+      .then(res => {
+        setArticles(res.data)
+        setMessage('Articles retrieved successfully')
+      })
+      .catch(err => {
+        if (err.response?.status === 401) {
+          redirectToLogin()
+        }
+        setMessage(err.response?.data?.message || 'Something went wrong')
+      })
+      .finally(() => {
+        setSpinnerOn(false)
+      })
   }
 
   const postArticle = article => {
@@ -54,15 +92,77 @@ export default function App() {
     // The flow is very similar to the `getArticles` function.
     // You'll know what to do! Use log statements or breakpoints
     // to inspect the response from the server.
+    setMessage('')
+    setSpinnerOn(true)
+    const token = localStorage.getItem('token')
+
+    axios.post(articlesUrl, article, {
+      headers: { Authorization: token }
+    })
+      .then(res => {
+        setArticles([...articles, res.data.article])
+        setMessage(res.data.message)
+      })
+      .catch(err => {
+        if (err.response?.status === 401) {
+          redirectToLogin()
+        }
+        setMessage(err.response?.data?.message || 'Something went wrong')
+      })
+      .finally(() => {
+        setSpinnerOn(false)
+      })
   }
 
   const updateArticle = ({ article_id, article }) => {
     // ✨ implement
     // You got this!
+    setMessage('')
+    setSpinnerOn(true)
+    const token = localStorage.getItem('token')
+
+    axios.put(`${articlesUrl}/${article_id}`, article, {
+      headers: { Authorization: token }
+    })
+      .then(res => {
+        setArticles(articles.map(art => 
+          art.article_id === article_id ? res.data.article : art
+        ))
+        setMessage(res.data.message)
+      })
+      .catch(err => {
+        if (err.response?.status === 401) {
+          redirectToLogin()
+        }
+        setMessage(err.response?.data?.message || 'Something went wrong')
+      })
+      .finally(() => {
+        setSpinnerOn(false)
+      })
   }
 
   const deleteArticle = article_id => {
     // ✨ implement
+    setMessage('')
+    setSpinnerOn(true)
+    const token = localStorage.getItem('token')
+
+    axios.delete(`${articlesUrl}/${article_id}`, {
+      headers: { Authorization: token }
+    })
+      .then(res => {
+        setArticles(articles.filter(art => art.article_id !== article_id))
+        setMessage(res.data.message)
+      })
+      .catch(err => {
+        if (err.response?.status === 401) {
+          redirectToLogin()
+        }
+        setMessage(err.response?.data?.message || 'Something went wrong')
+      })
+      .finally(() => {
+        setSpinnerOn(false)
+      })
   }
 
   return (
