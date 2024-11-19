a6c1fab572a4ada75b912aeb34775e7375d3d524
francesco damore <frandamore11@gmail.com>
2024-11-18 21:23:32 -0600

    implemented login form

--- frontend/components/LoginForm.js
+++ frontend/components/LoginForm.js
@@ -8,6 +8,7 @@ const initialFormValues = {
 export default function LoginForm(props) {
   const [values, setValues] = useState(initialFormValues)
   // ✨ where are my props? Destructure them here
+  const { login } = props // Destructuring login prop
 
   const onChange = evt => {
     const { id, value } = evt.target
@@ -17,6 +18,11 @@ export default function LoginForm(props) {
   const onSubmit = evt => {
     evt.preventDefault()
     // ✨ implement
+    login({
+      username: values.username.trim(),
+      password: values.password.trim()
+    })
+    setValues(initialFormValues) // Reset form after submission
   }
 
   const isDisabled = () => {
@@ -24,6 +30,11 @@ export default function LoginForm(props) {
     // Trimmed username must be >= 3, and
     // trimmed password must be >= 8 for
     // the button to become enabled
+    const { username, password } = values
+    return !(
+      username.trim().length >= 3 &&
+      password.trim().length >= 8
+    )
   }
 
   return (
